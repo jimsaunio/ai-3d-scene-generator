fa39d663c111e238e8cc520ed65c0b62cd779f41
jimsaunio <jim.saunio@student.howest.be>
2024-01-14 21:26:32 +0100

    Exporting scene and working on the correct size

--- js/script.js
+++ js/script.js
@@ -7,15 +7,42 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 const canvas = document.querySelector('#c');
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x000000);
+scene.background = new THREE.Color(0x808080);
 
 const size = {
-    width: window.innerWidth,
-    height: window.innerHeight
+    width: window.innerWidth - 100,
+    height: window.innerHeight - 100
 };
 
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 55;
+camera.position.y = 40;
+
+const loader = new GLTFLoader();
+
+loader.load('assets/scene.glb', function (gltf) {
+    gltf.scene.position.y = 50;
+    gltf.scene.scale.set(2, 2, 2);
+    scene.add(gltf.scene);
+    console.log(gltf);
+}
+);
+
+// position the loader on the scene
+
+const depthCameraSize = { width: 1920, height: 1920 };
+
+const depthCamera = new THREE.PerspectiveCamera(75, depthCameraSize.width / depthCameraSize.height, 0.1, 1000);
+
+// Adjust position based on Blender values
+depthCamera.position.x = 0; // Blender X
+depthCamera.position.y = 65.6817; // Blender Y
+depthCamera.position.z = 103.1674; // Blender Z
+
+// Adjust rotation bas
+
+
+
 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
@@ -24,8 +51,14 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+const planeGeometry = new THREE.PlaneGeometry(window.innerWidth, window.innerHeight);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xf4f4f4, side: THREE.DoubleSide });
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = Math.PI / 2 - 50;
+plane.position.y = -100;
+scene.add(plane);
+
+
 
 
 // Lights
@@ -33,11 +66,18 @@ controls.enableDamping = true;
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(10, 10, 10);
+scene.add(directionalLight);
+
+
+
+
 // render scene 
 
 const render = () => {
-    renderer.render(scene, camera);
-    controls.update();
+    renderer.render(scene, depthCamera);
     requestAnimationFrame(render);
 }
 
+render();
